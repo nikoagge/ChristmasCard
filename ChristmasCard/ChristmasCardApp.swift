81ab806f419cc14ddb8a1a05347245ba593dd6bc
nikoagge <nikoagge@gmail.com>
2023-12-26 17:34:12 +0200

    Initial Commit

---
+++ ChristmasCard/ChristmasCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasCardApp.swift
+//  ChristmasCard
+//
+//  Created by Nikos Aggelidis on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
